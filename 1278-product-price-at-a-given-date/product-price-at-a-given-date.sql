6ebc9c446e87d000cc87478131944e4fb4cf9d43
MOHAMMED ZAFEER K <145586766+Zafeerxyz@users.noreply.github.com>
2026-09-14 16:24:19 +0530

    Time: 378 ms (47.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+
+WITH RANKED_PRICE AS 
+(SELECT product_id ,
+        new_price, ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC)rn 
+        FROM products 
+        WHERE change_date <= '2019-08-16')
+SELECT p.product_id, 
+        COALESCE(r.new_price,10) AS price 
+    FROM (SELECT DISTINCT product_id FROM products) p 
+    LEFT JOIN RANKED_PRICE r
+        ON p.product_id = r.product_id
+    AND r.rn =1 ;
